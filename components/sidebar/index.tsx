d14d1d890d3456a3771861cd3c1227e201acebe8
Rodrigo Torres <rodrigoatorres7@gmail.com>
2026-03-11 11:56:15 -0300

    NavBar e Aside terminados

--- components/sidebar/index.tsx
+++ components/sidebar/index.tsx
@@ -1,7 +1,8 @@
 import { Sheet, SheetTrigger, SheetContent } from "@/components/ui/sheet";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-import { Home, Package, PanelBottom, SettingsIcon, ShoppingBag, Users } from "lucide-react";
+import { Home, LogOut, Package, PanelBottom, SettingsIcon, ShoppingBag, Users } from "lucide-react";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "../ui/tooltip";
 
 export function Sidebar() {
   return (
@@ -9,7 +10,103 @@ export function Sidebar() {
     {/* div Principal */}
     <div className="flex w-full flex-col bg-muted/40">
 
+        {/* Menu Desktop */}
+            <aside className="fixed inset-y-0 left-0 z-10 hidden w-14 border-r bg-background sm:flex flex-col">
+            <nav className="flex flex-col items-center gap-4 px-2 py-5 ">
+                <TooltipProvider>
+                    <Link 
+                    href="/"
+                    className="flex h-9 w-9 shrink-0 items-center justify-center bg-primary text-primary-foreground rounded-full"
+                    >
+                        <Package className="h-4 w-4" />
+                        <span className="sr-only">Dashboard Avatar</span>
+                    </Link>   
+                    {/* Icone Inicio */}
+                    <Tooltip>
+                        <TooltipTrigger asChild>
+                            <Link 
+                            href="#"
+                            className="flex h-9 w-9 shrink-0 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground"
+                            >
+                            <Home className="h-5 w-5" />
+                            <span className="sr-only">Inicio</span>
+                            </Link>   
+                        </TooltipTrigger>
+                        <TooltipContent side="right">Inicio</TooltipContent>
+                    </Tooltip>   
+                    {/* Icone Pedidos */}
+                    <Tooltip>
+                        <TooltipTrigger asChild>
+                            <Link 
+                            href="#"
+                            className="flex h-9 w-9 shrink-0 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground"
+                            >
+                            <Package className="h-5 w-5" />
+                            <span className="sr-only">Pedidos</span>
+                            </Link>   
+                        </TooltipTrigger>
+                        <TooltipContent side="right">Pedidos</TooltipContent>
+                    </Tooltip>  
+                    {/* Icone Produtos */}
+                    <Tooltip>
+                        <TooltipTrigger asChild>
+                            <Link 
+                            href="#"
+                            className="flex h-9 w-9 shrink-0 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground"
+                            >
+                            <ShoppingBag className="h-5 w-5" />
+                            <span className="sr-only">Produtos</span>
+                            </Link>   
+                        </TooltipTrigger>
+                        <TooltipContent side="right">Produtos</TooltipContent>
+                    </Tooltip>
+                    {/* Icone Clientes */}
+                    <Tooltip>
+                        <TooltipTrigger asChild>
+                            <Link 
+                            href="#"
+                            className="flex h-9 w-9 shrink-0 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground"
+                            >
+                            <Users className="h-5 w-5" />
+                            <span className="sr-only">Clientes</span>
+                            </Link>   
+                        </TooltipTrigger>
+                        <TooltipContent side="right">Clientes</TooltipContent>
+                    </Tooltip>
+                    {/* Icone Configurações */}
+                    <Tooltip>
+                        <TooltipTrigger asChild>
+                            <Link 
+                            href="#"
+                            className="flex h-9 w-9 shrink-0 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground"
+                            >
+                            <SettingsIcon className="h-5 w-5" />
+                            <span className="sr-only">Configurações</span>
+                            </Link>   
+                        </TooltipTrigger>
+                        <TooltipContent side="right">Configurações</TooltipContent>
+                    </Tooltip>
 
+                </TooltipProvider>
+            </nav>
+            {/* Sair */}
+            <nav className="mt-auto flex flex-col items-center gap-4 px-2 py-5">
+                <TooltipProvider>
+                     <Tooltip>
+                        <TooltipTrigger asChild>
+                            <Link 
+                            href="#"
+                            className="flex h-9 w-9 shrink-0 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground"
+                            >
+                            <LogOut className="h-5 w-5" />
+                            <span className="sr-only">Sair</span>
+                            </Link>   
+                        </TooltipTrigger>
+                        <TooltipContent side="right">Sair</TooltipContent>
+                    </Tooltip>
+                </TooltipProvider>    
+            </nav>
+            </aside>
 
         {/* Menu Mobile */}
       <div className="sm:hidden flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
